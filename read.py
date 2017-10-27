e8315ad07293e7f0a4deebaf43c18a0f76837a2d
Rodrigo <rodarima@gmail.com>
2017-10-27 22:32:39 +0200

    Add datasets and read test for wine.arff

---
+++ read.py
@@ -0,0 +1,19 @@
+from scipy.io import arff
+import numpy as np
+
+DATASET = "datasets/wine.arff"
+
+data, meta = arff.loadarff(DATASET)
+
+# Check for the class attribute
+if not 'class' in meta.names():
+	print('Error, the class attribute is not found.')
+	exit(1)
+
+names = meta.names()
+names.remove('class')
+
+data_noclass = np.array(data[names].tolist())
+
+# The dataset is now a matrix, and we can access the elements by row and column
+print(data_noclass[0])
