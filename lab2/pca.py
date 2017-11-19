648939d80ce976ad11cbb98e9eea940bbf358a36
Rodrigo <rodarima@gmail.com>
2017-11-19 22:05:49 +0100

    Add pca options for plot

--- lab2/pca.py
+++ lab2/pca.py
@@ -24,6 +24,9 @@ if len(sys.argv) != 2:
 #DATASET = "datasets/wine.arff"
 DATASET = sys.argv[1]
 GRAPH = True
+ERROR = False
+CORR = False #Plot covariance/correlation matrix?
+FIRST_COMP = False #Plot first components of X and Y?
 
 bn = os.path.basename(DATASET)
 img_fn = bn.replace('.arff', '.png')
@@ -59,14 +62,15 @@ mean_vec = np.matrix(np.mean(X, axis=0))
 n, m = X.shape
 M = np.repeat(mean_vec, n, axis=0)
 M = np.array(M)
-Xs = (X - M)
+Xc = (X - M) # Xc = X centered
 #sd = 1
-sd = np.std(Xs, axis=0)
-Xss = Xs/sd # Problem with division by 0
-Xss = np.nan_to_num(Xss)
+sd = np.std(Xc, axis=0)
+Xcs = Xc/sd # Xcs = X centered and scaled
+# Problem with division by 0
+Xcs = np.nan_to_num(Xcs)
 
-def PCA(Xs):
-	S = np.cov(Xs.T)
+def PCA(Xc):
+	S = np.cov(Xc.T)
 	eival, eivec = np.linalg.eig(S)
 
 	# Sort
@@ -85,75 +89,135 @@ def PCA(Xs):
 	k_fraq = np.sum(eival_bool)
 
 	k = max(3, k_fraq)
-	print(eival)
-	print(eival_cum)
-	print('k = {}'.format(k))
+	#print(eival)
+	#print(eival_cum)
+	#print('k = {}'.format(k))
 
 	keival = eival[0:k]
 	Zk = Z[:,0:k]
 
-	Yk = np.dot(Xs, Zk)
+	Yk = np.dot(Xc, Zk)
 
-	return Yk,keival
+	return Yk,keival,Zk
 
-#Yk, keival1 = PCA(Xs)
-Yks, keival = PCA(Xss)
+Yk, keival1, Zk = PCA(Xc)
+Yks, keival, Zks = PCA(Xcs)
 
+n_features = X.shape[1]
+k=keival.shape[0]
 skpca = skPCA(keival.shape[0])
-skpca.fit(Xss)
+skpca.fit(Xcs)
 res = skpca.explained_variance_
-skYk = skpca.transform(Xss)
+skYk = skpca.transform(Xcs)
 
 dist = round(np.linalg.norm(keival-res))
 
-print("Distance between our eigenvalues and SKlearn: {}".format(dist))
+#print("Distance between our eigenvalues and SKlearn: {}".format(dist))
 
-# Set the color map to match the number of species
-hot = plt.get_cmap('jet')
-cNorm  = colors.Normalize(vmin=0, vmax=len(classes))
-scalarMap = cmx.ScalarMappable(norm=cNorm, cmap=hot)
+# Project back the data
+Xc_ = np.dot(Yk, Zk.T) + M
+Xcs_ = np.dot(Yks, Zks.T)*sd + M
+
+from numpy.linalg import norm
+
+abs_cov = norm(X - Xc_)
+abs_cor = norm(X - Xcs_)
+
+rel_cov = abs_cov/norm(X)
+rel_cor = abs_cor/norm(X)
+
+#print("{}: R. err. cov={:.2f} cor={:.2f}. Abs. err cov={:.2f} cor={:.2f}".format(
+#	DATASET, rel_cov, rel_cor, abs_cov, abs_cor))
+
+absv0 = norm(X - Xc_, axis=0)
+absr0 = norm(X - Xcs_, axis=0)
+
+relv0 = absv0/norm(X, axis=0)
+relr0 = absr0/norm(X, axis=0)
+
+print("{}:  k/n = {}/{}. Mean rel error cov={}, corr={}".format(
+	DATASET, k, n_features, np.mean(relv0), np.mean(relr0)))
+
+if ERROR:
+
+	plt.close()
+	plt.plot(relv0, c='red', label='Covariance')
+	plt.plot(relr0, c='blue', label='Correlation')
+	plt.grid(True)
+	plt.title('Relative error by feature')
+	plt.legend()
+	#plt.show()
+	plt.savefig("fig/rel-error-by-feature-{}".format(img_fn))
+	plt.close()
+
+if FIRST_COMP:
+	# Set the color map to match the number of species
+	hot = plt.get_cmap('jet')
+	cNorm  = colors.Normalize(vmin=0, vmax=len(classes))
+	scalarMap = cmx.ScalarMappable(norm=cNorm, cmap=hot)
+
+	fig = plt.figure(figsize=(10, 5))
+
+	ax = fig.add_subplot(121)
+	ax.set_title('First two features of X')
+	ax.scatter(X[:,0], X[:,1], c=scalarMap.to_rgba(y))
+
+	ax = fig.add_subplot(122)
+	ax.set_title('First two components of the projected X')
+	ax.scatter(Yks[:,0], Yks[:,1], c=scalarMap.to_rgba(y))
+
+	plt.tight_layout()
+	plt.savefig('fig/components-wine.png')
+	#plt.show()
+	plt.close()
+
+if GRAPH:
+
+	# Set the color map to match the number of species
+	hot = plt.get_cmap('jet')
+	cNorm  = colors.Normalize(vmin=0, vmax=len(classes))
+	scalarMap = cmx.ScalarMappable(norm=cNorm, cmap=hot)
 
 
-fig = plt.figure(figsize=(15, 10))
+	fig = plt.figure(figsize=(15, 10))
 
-ax = fig.add_subplot(231)
-ax.set_title('SKlearn. Axis 0 and 1')
-ax.scatter(skYk[:,0], skYk[:,1], c=scalarMap.to_rgba(y))
+	ax = fig.add_subplot(231)
+	ax.set_title('SKlearn. Axis 0 and 1')
+	ax.scatter(skYk[:,0], skYk[:,1], c=scalarMap.to_rgba(y))
 
-ax = fig.add_subplot(232)
-ax.set_title('SKlearn. Axis 1 and 2')
-ax.scatter(skYk[:,1], skYk[:,2], c=scalarMap.to_rgba(y))
+	ax = fig.add_subplot(232)
+	ax.set_title('SKlearn. Axis 1 and 2')
+	ax.scatter(skYk[:,1], skYk[:,2], c=scalarMap.to_rgba(y))
 
-ax = fig.add_subplot(233)
-ax.set_title('SKlearn. Axis 0 and 2')
-ax.scatter(skYk[:,0], skYk[:,2], c=scalarMap.to_rgba(y))
+	ax = fig.add_subplot(233)
+	ax.set_title('SKlearn. Axis 0 and 2')
+	ax.scatter(skYk[:,0], skYk[:,2], c=scalarMap.to_rgba(y))
 
-#ax = fig.add_subplot(334)
-#ax.set_title('Without std scaling. Axis 0 and 1')
-#ax.scatter(Yk[:,0], Yk[:,1], c=scalarMap.to_rgba(y))
+	#ax = fig.add_subplot(334)
+	#ax.set_title('Without std scaling. Axis 0 and 1')
+	#ax.scatter(Yk[:,0], Yk[:,1], c=scalarMap.to_rgba(y))
 
-#ax = fig.add_subplot(335)
-#ax.set_title('Without std scaling. Axis 1 and 2')
-#ax.scatter(Yk[:,1], Yk[:,2], c=scalarMap.to_rgba(y))
+	#ax = fig.add_subplot(335)
+	#ax.set_title('Without std scaling. Axis 1 and 2')
+	#ax.scatter(Yk[:,1], Yk[:,2], c=scalarMap.to_rgba(y))
 
-#ax = fig.add_subplot(336)
-#ax.set_title('Without std scaling. Axis 0 and 2')
-#ax.scatter(Yk[:,0], Yk[:,2], c=scalarMap.to_rgba(y))
+	#ax = fig.add_subplot(336)
+	#ax.set_title('Without std scaling. Axis 0 and 2')
+	#ax.scatter(Yk[:,0], Yk[:,2], c=scalarMap.to_rgba(y))
 
-ax = fig.add_subplot(234)
-ax.set_title('With std scaling. Axis 0 and 1')
-ax.scatter(Yks[:,0], Yks[:,1], c=scalarMap.to_rgba(y))
+	ax = fig.add_subplot(234)
+	ax.set_title('With std scaling. Axis 0 and 1')
+	ax.scatter(Yks[:,0], Yks[:,1], c=scalarMap.to_rgba(y))
 
-ax = fig.add_subplot(235)
-ax.set_title('With std scaling. Axis 1 and 2')
-ax.scatter(Yks[:,1], Yks[:,2], c=scalarMap.to_rgba(y))
+	ax = fig.add_subplot(235)
+	ax.set_title('With std scaling. Axis 1 and 2')
+	ax.scatter(Yks[:,1], Yks[:,2], c=scalarMap.to_rgba(y))
 
-ax = fig.add_subplot(236)
-ax.set_title('With std scaling. Axis 0 and 2')
-ax.scatter(Yks[:,0], Yks[:,2], c=scalarMap.to_rgba(y))
+	ax = fig.add_subplot(236)
+	ax.set_title('With std scaling. Axis 0 and 2')
+	ax.scatter(Yks[:,0], Yks[:,2], c=scalarMap.to_rgba(y))
 
 
-plt.tight_layout()
-plt.savefig(img_path)
-#plt.show()
-
+	plt.tight_layout()
+	plt.savefig(img_path)
+	#plt.show()
