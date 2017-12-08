a2adf256f49f59641be21a0063092ba57681a985
Rodrigo <rodarima@gmail.com>
2017-12-08 18:52:58 +0100

    Add kNN with classes

--- lab3/lazy.py
+++ lab3/lazy.py
@@ -4,7 +4,6 @@ import matplotlib.pyplot as plt
 import matplotlib.colors as colors
 import matplotlib.cm as cmx
 import pandas as pd
-import math
 import sys, os.path
 
 np.random.seed(1)
@@ -65,40 +64,85 @@ def do_getData(dataset_path, dataset_name, i, data_type):
 	Xcs = Xc/sd # Xcs = X centered and scaled
 	# Problem with division by 0
 	Xcs = np.nan_to_num(Xcs)
-	return Xcs, Xn
+	return Xcs, Xn, y
 
-def do_kNNAlgorithm(training_set, testing_instance, k, training_set_classes):
-	e_distances = []
-	for i in range(training_set.shape[0]):
-		#e_distances[i] = np.linalg.norm(train_matrix[i] - testing_instance)
-		e_distance = np.linalg.norm(training_set[i] - testing_instance)
-		e_distances.append((training_set[i], e_distance, training_set_classes[i]))
+# Select the most common class
+def select_most_common_class(classes):
+	#print(classes)
+	uniq_classes, counts = np.unique(classes, return_counts=True)
+	#print(counts)
+	return uniq_classes[np.argmax(counts)]
 
-	e_distances.sort(key=lambda x: x[1])
-	neighbors = e_distances[:int(k)]
-	return neighbors
+def do_kNNAlgorithm(training_set, testing_instance, conf, training_set_classes):
+	k, select_f, distance_f = conf
+	distances = training_set - testing_instance
+	norms = np.linalg.norm(distances, axis=1)
+	sorted_indices = np.argsort(norms)
+	classes = training_set_classes[sorted_indices[0:k]]
+
+	selected_class = select_most_common_class(classes)
+
+	#e_distances = []
+	#for i in range(training_set.shape[0]):
+	#	#e_distances[i] = np.linalg.norm(train_matrix[i] - testing_instance)
+	#	e_distance = np.linalg.norm(training_set[i] - testing_instance)
+	#	e_distances.append((training_set[i], e_distance, training_set_classes[i]))
+
+	#e_distances.sort(key=lambda x: x[1])
+	##np.argsort(e_distances)
+	#neighbors = e_distances[:int(k)]
+
+	#class_name, counts = np.unique(training_set_classes, return_counts=True)
+	#selected_class = class_name[np.argmax(counts)]
+
+	return selected_class
 
 #Fills training and test
 dataset_name = DATASET.split("/")[-1] + ".fold.00000"
+print(dataset_name)
 train = [0] * N_FOLD
 testing = [0] * N_FOLD
+train_nominal = [0] * N_FOLD
+testing_nominal = [0] * N_FOLD
 train_classes = [0] * N_FOLD
 testing_classes = [0] * N_FOLD
 for i in range(N_FOLD):
-	train[i], train_classes[i] = do_getData(DATASET, dataset_name, i, 'train')
-	testing[i], testing_classes[i] = do_getData(DATASET, dataset_name, i, 'test')
+	train[i], train_nominal[i], train_classes[i] = do_getData(DATASET, dataset_name, i, 'train')
+	testing[i], testing_nominal[i], testing_classes[i] = do_getData(DATASET, dataset_name, i, 'test')
 
 #print('trainMatrix: {} \n'.format(train))
 #print('train classes: {} \n'.format(train_classes))
 #print('testMatrix: {} \n'.format(testing))
 #print('test classes: {} \n'.format(testing_classes))
 
-neighbors = [[0] * N_FOLD] * N_FOLD
-for i in range(N_FOLD):
+for i in range(1):
+	N_TEST = testing[i].shape[0]
+	N_TRAIN = train[i].shape[0]
+
+	train_block = train[i]
+	test_block = testing[i]
+	train_classes_block = train_classes[i]
+	test_classes_block = testing_classes[i]
+
 	#For K we can use the sqr root of the number of samples, an make it odd to reduce the chance
 	#of a tie vote
-	k=np.ceil(math.sqrt(train[i].shape[0]) // 2 * 2 + 1)
-	for j in range(N_FOLD):
-		neighbors[i][j] = do_kNNAlgorithm(train[i], testing[i][j], k, train_classes[i])
+	#k = int(np.sqrt(N_TRAIN) // 2 * 2 + 1)
+	k=3
 
-print('neighbors: {} \n'.format(neighbors))
+	classified = np.empty([N_TEST])
+	for j in range(N_TEST):
+		selected_point = test_block[j]
+		expected_class = test_classes_block[j]
+		classified[j] = do_kNNAlgorithm(train_block, selected_point, k, train_classes_block)
+	
+	
+	correct = (classified == test_classes_block)
+	print('{:.3f}'.format(100*np.sum(correct)/N_TEST))
+
+	
+	
+
+
+	
+
+#print('neighbors: {} \n'.format(neighbors))
